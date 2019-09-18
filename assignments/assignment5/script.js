489d699a0c0698126b7b2e2f0e4faf38428d1f2f
Aubrey Edmonds <dedmonds@email.sc.edu>
2019-09-18 17:37:28 -0400

    completed assignment 5 first try

---
+++ assignments/assignment5/script.js
@@ -0,0 +1,13 @@
+const name = prompt("Please enter your name, friend");
+const coffees = prompt("How many coffees would you like?");
+const tip = prompt("Tip?? Please?? :)");
+
+const price = coffees * 1.50;
+const tax = price * .07;
+const cTip = parseFloat(tip);
+const totalP = parseFloat(price + tax + cTip);
+const totalPrice = parseFloat(totalP.toFixed(2));
+
+let total = document.getElementById("total");
+
+total.innerHTML = name + " purchased " + coffees + " coffees. Their total is: $" + totalPrice + ". Thank you! Please come again :)";
