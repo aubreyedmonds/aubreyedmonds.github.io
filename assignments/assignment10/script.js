9cfe4ee9285ac73f1fe44e3906f19f837494aebe
aubreyedmonds <54610912+aubreyedmonds@users.noreply.github.com>
2019-10-23 10:15:08 -0400

    adding quote function to assignment 10

--- assignments/assignment10/script.js
+++ assignments/assignment10/script.js
@@ -1,9 +1,19 @@
 "using strict"
 
-function box1(){ //quotes
-    console.log("testing");
+function quoteFunction(){
+    //array of quotes
+    var quoteArray = ["bing","bong","boom","box","beep"];
+    var i = 0;
+    //display a quote under the header in the span id
+    setInterval(function(){
+        document.getElementById('Quotes').innerHTML = quoteArray[i++];
+        //go back to beginning if necessary
+        if(i == quoteArray.length){
+            i=0;
+        }
+    }, 2000);
 }
 
 window.onload = function(){
-    
+    this.quoteFunction();
 }
