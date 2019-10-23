bf054c945bf36660b60e45a23d4fc6bca19d7b91
aubreyedmonds <54610912+aubreyedmonds@users.noreply.github.com>
2019-10-23 10:29:39 -0400

    testing push

--- assignments/assignment10/script.js
+++ assignments/assignment10/script.js
@@ -2,7 +2,7 @@
 
 function quoteFunction(){
     //array of quotes
-    var quoteArray = ["Years of love have been forgot, in the hatred of a minute. - Edgar Allan Poe","You cannot conquer a free man. The most you can do is kill him. - Robert Heinlein","The hour of departure has arrived, and we go our ways. I to die, and you to live. Which is better, God only knows. - Socrates","A ship is safe in a harbor. But that's not what ships are for.","How lucky I am to have something that makes saying goodbye so hard. - Winnie the Pooh"];
+    var quoteArray = ["Years of love have been forgot, in the hatred of a minute. - Edgar Allan Poe","You cannot conquer a free man. The most you can do is kill him. - Robert Heinlein","The hour of departure has arrived, and we go our ways. I to die, and you to live. Which is better, God only knows. - Socrates","A ship is safe in a harbor. But that's not what ships are for.","How lucky I am to have something that makes saying goodbye so hard. - Winnie the Pooh :)"];
     var i = 0;
     //display a quote under the header in the span id
     setInterval(function(){
