06b9fe6dbad57fcbccde346737f15b0c7db0c083
aubreyedmonds <54610912+aubreyedmonds@users.noreply.github.com>
2019-11-21 11:52:04 -0500

    editing assignment 16

--- assignments/assignment16/public/script.js
+++ assignments/assignment16/public/script.js
@@ -110,6 +110,7 @@ async function addPie(){
 
     async function editPie(){
         let id = document.getElementById("edit-pie-id").textContent;
+        // let id = document.getElementById("edit-pie-id").value;
         let crust = document.getElementById("txt-edit-pie-crust").value;
         let flavor = document.getElementById("txt-edit-pie-flavor").value;
         let filling = document.getElementById("txt-edit-pie-filling").value;
@@ -143,3 +144,149 @@ window.onload = function(){
     let editPieButton = document.getElementById("btn-edit-pie");
     editPieButton.onclick = editPie;
 }
+
+// async function showPies(){
+//     let piesJson = await fetch('api/pies');
+//     let pies = await piesJson.json();
+//     let piesDiv = document.getElementById("pies");
+//     piesDiv.innerHTML = "";
+
+//     for(i in pies){
+//         piesDiv.append(getPieElem(pies[i]));
+//     }
+// }
+
+// async function showPie(){
+//     let id = document.getElementById("txt-pie-id").value;
+//     let response = await fetch(`api/pies/${id}`);
+//     let pie = await response.json();
+
+//     let pieDiv = document.getElementById("pie");
+//     pieDiv.append(getPieElem(pie));
+// }
+
+// function getPieElem(pie){
+//     let pieDiv = document.createElement("div");
+//     pieDiv.classList.add("pie");
+//     let pieTitle = document.createElement("h3");
+//     pieTitle.innerHTML = pie.id + ": " + pie.name;
+
+//     let pieP = document.createElement("p");
+//     pieP.innerHTML = `has the flavor ${pie.flavor}! It is filled with ${pie.filling}, topped with ${pie.topping}, and has a ${pie.crust} crust. Absolutely delicious!`;
+
+//     //create edit and delete links
+//     let editLink = document.createElement("a");
+//     editLink.href = "#edit-pie-form";
+//     editLink.innerHTML = "Edit";
+//     editLink.setAttribute("data-id", pie.id);
+//     editLink.onclick =showEditPie;
+//     let deleteLink = document.createElement("a");
+//     deleteLink.href = "#";
+//     deleteLink.innerHTML = "Delete";
+//     deleteLink.setAttribute("data-id", pie.id);
+//     deleteLink.onclick = deletePie;
+//     pieP.append(editLink);
+//     pieP.append(deleteLink);
+
+//     pieDiv.append(pieCrust);
+//     pieP.append(pieP);
+    
+//     return pieDiv;
+// }
+
+// async function showEditPie(){
+//     const id = this.getAttribute("data-id");
+//     document.getElementById("edit-pie-id").innerHTML = id;
+
+//     let response = await fetch(`api/pies/${id}`);
+//     let pie = await response.json();
+//     document.getElementById("txt-edit-pie-crust").value = pie.crust;
+//     document.getElementById("txt-edit-pie-flavor").value = pie.flavor;
+//     document.getElementById("txt-edit-pie-filling").value = pie.filling;
+//     document.getElementById("txt-edit-pie-topping").value = pie.topping;
+
+//     return false;
+// }
+
+// async function deletePie(){
+//     const id = this.getAttribute("data-id");
+    
+//     let response = await fetch(`/api/pies/${id}`, {
+//         method: 'DELETE',
+//         headers: {
+//         'Content-Type': 'application/json;charset=utf-8',
+//         }
+//     });
+
+//     if(response.status != 200){
+//         console.log("Error adding pie");
+//         return;
+//     }
+
+//     showPies();
+//     return false;
+// }
+
+// async function addPie(){
+//     //get the pie inforamtion
+//     const pieCrust = document.getElementById("txt-new-pie-crust").value;
+//     const pieFlavor = document.getElementById("txt-new-pie-flavor").value;
+//     const pieFilling = document.getElementById("txt-new-pie-filling").value;
+//     const pieTopping = document.getElementById("txt-new-pie-topping").value;
+
+//     console.log(`you are adding ${pieCrust}, ${pieFlavor}, ${pieFilling}, ${pieTopping}`);
+
+//     let pie = {"Crust": pieCrust, "Flavor":pieFlavor, "Filling":pieFilling, "Topping":pieTopping};
+
+//     let response = await fetch('/api/pies/', {
+//         method: 'POST',
+//         headers: {
+//         'Content-Type': 'application/json;charset=utf-8',
+//         },
+//         body: JSON.stringify(pie),
+//     });
+
+//     if(response.status != 200){
+//         console.log("Error adding pie");
+//         return;
+//     }
+
+//     let result = await response.json();
+//     showPies();
+// }
+
+// async function editPie(){
+//     let id = document.getElementById("edit-pie-id").textContent;
+//     let crust = document.getElementById("txt-edit-pie-crust").value;
+//     let flavor = document.getElementById("txt-edit-pie-flavor").value;
+//     let filling = document.getElementById("txt-edit-pie-filling").value;
+//     let topping = document.getElementById("txt-edit-pie-topping").value;
+//     let pie = {"crust":crust, "flavor": flavor, "filling": filling, "topping": topping};
+
+//     let response = await fetch(`/api/pies/${id}`, {
+//         method: 'PUT',
+//         headers: {
+//         'Content-Type': 'application/json;charset=utf-8',
+//         },
+//         body: JSON.stringify(pie),
+//     });
+
+//     if(response.status != 200){
+//         console.log("Error editing pie");
+//     }
+
+//     //update the song list
+//     showPies();
+// }
+
+// window.onload = function(){
+//     this.showPies();
+//     let showSongButton = document.getElementById("btn-show-pie");
+//     showPieButton.onclick = showPie;
+
+//     let addPieButton = document.getElementById("btn-add-pie");
+//     addPieButton.onclick = addPie;
+
+//     let editPieButton = document.getElementById("btn-edit-pie");
+//     editPieButton.onclick = this.editPie;
+// }
